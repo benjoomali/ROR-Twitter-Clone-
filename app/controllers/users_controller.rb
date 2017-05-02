81a28ceb400b80700af4b5b7cccdcafbc825900d
benjoomali <benjoomali@gmail.com>
2017-05-02 18:21:46 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,9 +20,9 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to Bitter."
-      redirect_to user_url(@user) #or equivalently => @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your Bitter Account."
+      redirect_to root_url
     else 
       render 'new'
     end 
